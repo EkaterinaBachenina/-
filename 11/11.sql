aa0136cb126399055b2e99e52514fec6e2080bec
EkaterinaBachenina <midariucihag59@gmail.com>
2025-12-26 15:53:41 +0500

    Add files via upload

---
+++ 11/11.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Publishers (
+    publisher_id INT PRIMARY KEY,
+    publisher_name VARCHAR(255),
+    publisher_city VARCHAR(100)
+);
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    author_name VARCHAR(255),
+    publisher_id INT,
+    FOREIGN KEY (publisher_id) REFERENCES Publishers(publisher_id)
+);
+
+
+CREATE TABLE CarModels (
+    car_model VARCHAR(100) PRIMARY KEY,
+    car_manufacturer VARCHAR(100)
+);
+
+
+CREATE TABLE RaceResults (
+    race_id INT,
+    driver_id INT,
+    car_model VARCHAR(100),
+    finish_time TIME,
+    PRIMARY KEY (race_id, driver_id),
+    FOREIGN KEY (car_model) REFERENCES CarModels(car_model)
+);
+
+CREATE TABLE RoomBookings (
+    booking_time DATETIME,
+    room_name VARCHAR(100),
+    event_name VARCHAR(255),
+    event_type VARCHAR(50),
+    PRIMARY KEY (booking_time, room_name),
+    FOREIGN KEY (event_type) REFERENCES EventTypes(event_type)
+);
+
+CREATE TABLE EventTypes (
+    event_type VARCHAR(50) PRIMARY KEY,
+    room_name VARCHAR(100)
+);
+
